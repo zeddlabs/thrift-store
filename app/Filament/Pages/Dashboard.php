45c4d4d94f76474386892fd34738a29386ce2fda
Mhd. Zulfikar Pinem <zulfikarm022@gmail.com>
2023-12-13 12:12:55 +0700

    style: customize dashboard page

---
+++ app/Filament/Pages/Dashboard.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Infolists\Components\Section;
+use Filament\Panel;
+
+class Dashboard extends \Filament\Pages\Dashboard
+{
+  protected static ?string $navigationLabel = 'Dashboard';
+  protected static ?string $title = 'Dashboard';
+}
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers\Filament;
 
 use App\Filament\Pages\Auth\Login;
+use App\Filament\Pages\Dashboard;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -34,7 +35,7 @@ class AdminPanelProvider extends PanelProvider
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
-                Pages\Dashboard::class,
+                Dashboard::class,
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([

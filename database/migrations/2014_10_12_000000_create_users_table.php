d6a9808e8830c9f3f72c875914914424068dbe73
Mhd. Zulfikar Pinem <zulfikarm022@gmail.com>
2023-12-18 08:36:25 +0700

    refactor: remove whatsapp field

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('whatsapp');
             $table->timestamps();
         });
     }

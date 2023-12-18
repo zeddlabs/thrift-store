85ca01c26d178c433bd8f5405f74897ec322188e
Mhd. Zulfikar Pinem <zulfikarm022@gmail.com>
2023-12-18 09:02:07 +0700

    feat: CRUD customers

---
+++ app/Filament/Resources/CustomerResource.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CustomerResource\Pages;
+use App\Filament\Resources\CustomerResource\RelationManagers;
+use App\Models\Customer;
+use Filament\Forms;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CustomerResource extends Resource
+{
+    protected static ?string $model = Customer::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-user-group';
+    protected static ?string $navigationLabel = 'Pelanggan';
+    protected static ?int $navigationSort = 4;
+    protected static ?string $modelLabel = 'Data Pelanggan';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make('Form Pelanggan')
+                    ->schema([
+                        TextInput::make('name')
+                            ->label('Nama')
+                            ->autofocus()
+                            ->required()
+                            ->placeholder('Nama Pelanggan'),
+                        TextInput::make('whatsapp')
+                            ->label('Whatsapp')
+                            ->required()
+                            ->placeholder('Whatsapp Pelanggan'),
+                        TextInput::make('email')
+                            ->label('Email')
+                            ->required()
+                            ->placeholder('Email Pelanggan')
+                            ->email(),
+                        TextInput::make('password')
+                            ->label('Password')
+                            ->required()
+                            ->placeholder('Password Pelanggan')
+                            ->password(),
+                    ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label('Nama')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('whatsapp')
+                    ->label('Whatsapp')
+                    ->searchable(),
+                TextColumn::make('email')
+                    ->label('Email')
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCustomers::route('/'),
+            'create' => Pages\CreateCustomer::route('/create'),
+            'edit' => Pages\EditCustomer::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/CustomerResource/Pages/EditCustomer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Filament\Resources\CustomerResource\Pages;
+
+use App\Filament\Resources\CustomerResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditCustomer extends EditRecord
+{
+    protected static string $resource = CustomerResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}

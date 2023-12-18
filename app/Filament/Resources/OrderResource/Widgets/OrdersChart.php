fc248810a6def6a76ffa247ca09ae255db8396e8
Mhd. Zulfikar Pinem <zulfikarm022@gmail.com>
2023-12-18 22:13:41 +0700

    feat: create orders chart widget

---
+++ app/Filament/Resources/OrderResource/Widgets/OrdersChart.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Filament\Resources\OrderResource\Widgets;
+
+use App\Models\Order;
+use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+use Illuminate\Support\Carbon;
+
+class OrdersChart extends ChartWidget
+{
+    protected int | string | array $columnSpan = 'full';
+    protected static ?string $maxHeight = '300px';
+    protected static ?string $heading = 'Pesanan Tahun Ini';
+
+    protected function getData(): array
+    {
+        $data = Trend::model(Order::class)
+            ->between(
+                start: now()->startOfYear(),
+                end: now()->endOfYear(),
+            )
+            ->perMonth()
+            ->count();
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Jumlah Pesanan',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $data->map(fn (TrendValue $value) => Carbon::parse($value->date)->format('M')),
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'line';
+    }
+}

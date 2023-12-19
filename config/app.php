e42a974ea19a74971037769978fdba1193393e49
Mhd. Zulfikar Pinem <zulfikarm022@gmail.com>
2023-12-19 15:45:38 +0700

    feat: register Number utility class

--- config/app.php
+++ config/app.php
@@ -184,6 +184,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        Illuminate\Support\Number::class
     ])->toArray(),
 
 ];
